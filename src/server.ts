ca92f73854ce52ae0652ac9d2859b758a81f50ce
Lucas França <lucasvpessoafranca@gmail.com>
2023-06-27 23:06:03 -0300

    init view

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,9 @@ import mustache from 'mustache-express'
 import path from "path"
 import mainRoutes from './routes/index'
 
-
 const server = express()
+server.use(express.static(path.join(__dirname, '../public')));
+
 server.use(mainRoutes)
 
 server.use((req, res) => {
@@ -20,6 +21,8 @@ server.set('view engine', 'mustache')
 server.set('views', path.join(__dirname, "views"))
 server.engine('mustache', mustache())
 
-server.use(express.static(path.join(__dirname, '../public')))
 
+
+
+// server.use(express.static(path.join(__dirname, 'public')));
 server.listen(process.env.PORT);
