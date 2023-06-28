99480a4d5aa5f967e6453d11ab0b1112f962131f
Lucas França <lucasvpessoafranca@gmail.com>
2023-06-28 20:48:09 -0300

    ADD: active-menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
-
+import {createMenuObject} from '../helpers/createMenuObject';
 export  const home = (req: Request, res: Response) => {
     res.render('pages/page', {
+        // menu: {
+        //     all:true,
+        //     dogs:false,
+        //     cats:false,
+        //     fishes:false,
+        // },
+        menu: createMenuObject("all"),
         banner: {
             title:"todos os animais",
             background:"allanimals.jpg"
@@ -12,6 +19,13 @@ export  const home = (req: Request, res: Response) => {
 }
 export  const dogs = (req: Request, res: Response) => {
     res.render('pages/page', {
+        // menu: {
+        //     all:false,
+        //     dogs:true,
+        //     cats:false,
+        //     fishes:false,
+        // }
+        menu: createMenuObject("dogs"),
         banner: {
             title:"Cachorros",
             background:"banner_dog.jpg"
@@ -22,6 +36,13 @@ export  const dogs = (req: Request, res: Response) => {
 
 export  const cats = (req: Request, res: Response) => {
     res.render('pages/page', {
+        // menu: {
+        //     all:false,
+        //     dogs:false,
+        //     cats:true,
+        //     fishes:false,
+        // },
+        menu: createMenuObject("cats"),
         banner: {
             title:"Gatos",
             background:"banner_cat.jpg"
@@ -34,6 +55,14 @@ export  const cats = (req: Request, res: Response) => {
 export  const fishes = (req: Request, res: Response) => {
     
     res.render('pages/page', {
+        // menu: {
+        //     all:false,
+        //     dogs:false,
+        //     cats:false,
+        //     fishes:true,
+        // },
+        menu: createMenuObject("fishes"),
+
         banner: {
             title:"Peixes",
             background:"banner_fish.jpg"
